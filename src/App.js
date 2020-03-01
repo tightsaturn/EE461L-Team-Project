4ad16f18c5260f86141a2c99339eb861fb15154c
Jimmy Phan <jimmyphan777@gmail.com>
2020-03-01 02:55:54 -0600

    finished the top navbar for home page

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import Header from './models/Header.js'
+import Body from './models/Body.js'
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Body/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/models/Body.js
@@ -0,0 +1,15 @@
+import React from "react"
+import 'bootstrap/dist/css/bootstrap.min.css'
+import "../App.css"
+import foodimg1 from "../images/foodimg1.jpg"
+import background from "../images/background.jpg"
+
+var Body = () => {
+    return (
+        <div className="container-fluid">
+            Hello
+        </div>
+    )
+}
+
+export default Body
