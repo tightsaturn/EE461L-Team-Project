5bf4c3a8e54acdeb958274b1bb9db2416f2f69fb
JaimeTan <32779789+JaimeTan@users.noreply.github.com>
2020-03-05 13:18:18 -0600

    Add files via upload
    Fixed Navigation and names of Files

---
+++ components/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Homepage</h1>
+            <p>Home</p>
+        </div>
+    );
+}
+
+export default Homepage;
---
+++ components/PokemonLocation.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+
+const AnyReactComponent = ({ text }) => <div>{text}</div>;
+
+class PokemonLocation extends Component {
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 11
+    };
+
+    render() {
+        return (
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <AnyReactComponent
+                        lat={30.2672}
+                        lng={97.7431}
+                        text="My Marker"
+                    />
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+export default PokemonLocation;
