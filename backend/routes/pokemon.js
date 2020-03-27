f4e5c68816222210ba0ca8a02c3d104c04c5549e
Tristan McDaniel <t.mcdaniel511@utexas.edu>
2020-03-27 07:50:38 -0500

    Added the ability to search for pokemon by id

--- backend/routes/pokemon.js
+++ backend/routes/pokemon.js
@@ -1,19 +1,24 @@
 const router = require('express').Router();
 let Pokemon = require('../models/pokemon.model');           // link and require the mongoose model created for the Pokemon database
 
+// -------------------------------------------------------------
 // Setup paths that can be called and the action that will occur
+// -------------------------------------------------------------
+
+// Get all Pokemon in the database
 router.route('/').get((req, res) => {               // Generic url with /pokemon/
     console.log("URL /pokemon/ has been called");
-    Pokemon.findOne({}, function (error, documents) {
-        console.log(JSON.stringify(documents))
-    });
     Pokemon.find()                                  // Finds all pokemon in database
         .then(pokemon => res.json(pokemon))         // Returns JSON with list of all pokemon
         .catch(err => res.status(400).json('Error: ' + err));
 });
 
-// router.route(`/:id`).get((req, res) => {
-//     console.log("URL /pokemon/" + req.param.id + " has been called");
-// })
+// Get a Pokemon by the id specified by PokeAPI (id field in pokemon database)
+router.route(`/:id`).get((req, res) => {
+    console.log("URL /pokemon/" + req.params.id + " has been called");
+    Pokemon.findOne({id: req.params.id})
+        .then(pokemon => res.json(pokemon))
+        .catch(err => res.status(400).json('Error: ' + err));
+});
 
 module.exports = router;
