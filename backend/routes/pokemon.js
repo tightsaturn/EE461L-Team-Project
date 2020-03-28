6c5f256131e7b09aff24ea45a858ec27c2651e4f
Tristan McDaniel <t.mcdaniel511@utexas.edu>
2020-03-28 16:53:33 -0500

    Displaying pokemon for adding team member in progress

--- backend/routes/pokemon.js
+++ backend/routes/pokemon.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
 let Pokemon = require('../models/pokemon.model');           // link and require the mongoose model created for the Pokemon database
 
+async function getPokemonInRange(props) {
+    var result = [];
+    
+    console.log("Getting Pokemon:");
+    console.log("----------------");
+    for (let i = props.start_id; i <= props.end_id; i++) {
+        await Pokemon.findOne({id: i})
+            .then(pokemon => {
+                result.push(pokemon)
+                console.log(result)
+            })
+    }
+
+    console.log(result);
+    return result;
+}
+
 // -------------------------------------------------------------
 // Setup paths that can be called and the action that will occur
 // -------------------------------------------------------------
@@ -19,6 +36,41 @@ router.route(`/:id`).get((req, res) => {
     Pokemon.findOne({id: req.params.id})
         .then(pokemon => res.json(pokemon))
         .catch(err => res.status(400).json('Error: ' + err));
+});
+
+// Get a list of Pokemon within the inclusive range provided. Range is in terms of the id specified
+// by PokeAPI (id field in pokemon database)
+router.route('/:start_id/:end_id').get((req, res) => {
+    console.log("URL /pokemon/" + req.params.start_id + "/" + req.params.end_id + " has been called");
+    var result = [];
+    var status = [];
+    for (let i = 0; i < (req.params.end_id - req.params.start_id); i++) {
+        status.push(false);
+    }
+    
+    console.log("Getting Pokemon:");
+    console.log("----------------");
+    for (let i = req.params.start_id; i <= req.params.end_id; i++) {
+        Pokemon.findOne({id: i})
+            .then(pokemon => {
+                result.push(pokemon)
+                status[req.params.end_id - i] = true
+            })
+            .catch(err => res.status(400).json('Error: ' + err));
+    }
+
+    for (let i = 0; i < (req.params.end_id - req.params.start_id); i++) {
+        if (status[i] == false) {
+            i = 0;
+        }
+    }
+
+    console.log("result:");
+    console.log("---------------------------------------------");
+    console.log(result);
+    // res.json(result);
+    res = result;
+    //res = getPokemonInRange(req.params.start_id, req.params.end_id);
 });
 
 module.exports = router;
