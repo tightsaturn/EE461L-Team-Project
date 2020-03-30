2113f5a0684a65930ca7ef513a07be93a25092bc
Tristan McDaniel <t.mcdaniel511@utexas.edu>
2020-03-29 22:12:36 -0500

    Team builder mostly done

--- backend/routes/pokemon.js
+++ backend/routes/pokemon.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 let Pokemon = require('../models/pokemon.model');           // link and require the mongoose model created for the Pokemon database
+const mongoose = require('mongoose');
 
 async function getPokemonInRange(props) {
     var result = [];
@@ -59,45 +60,18 @@ router.route('/totalNumPokemon').get((req, res) => {
 
 // Get a list of Pokemon within the inclusive range provided. Range is in terms of the id specified
 // by PokeAPI (id field in pokemon database)
-// NOTE: This function does NOT work. DO NOT USE FOR NOW. The problem is that the array that
-//       is populated by the pokemon objects is returned as empty before the mongoose
-//       queries are able to finish. This is due to the fact that the queries seem to be
-//       asynchronous and will just occur whenever. In other routes, the function has to
-//       wait on the queries because the response is the result of the query. Here, the
-//       result array that is returned that is supposed to hold all of the results of the
-//       queries exists without the queries so the function returns the array without
-//       waiting on the queries to finish.
-router.route('/:start_id/:end_id').get((req, res) => {
+router.route('/:start_id/:end_id').get(async (req, res) => {
     console.log("URL /pokemon/" + req.params.start_id + "/" + req.params.end_id + " has been called");
-    var result = [];
-    var status = [];
-    for (let i = 0; i < (req.params.end_id - req.params.start_id); i++) {
-        status.push(false);
-    }
+    var result_array = [];
     
-    console.log("Getting Pokemon:");
-    console.log("----------------");
+    var id_array = [];
     for (let i = req.params.start_id; i <= req.params.end_id; i++) {
-        Pokemon.findOne({id: i})
-            .then(pokemon => {
-                result[pokemon.id - req.params.start_id] = pokemon
-                status[req.params.end_id - i] = true
-            })
-            .catch(err => res.status(400).json('Error: ' + err));
+        id_array.push(i);
     }
 
-    for (let i = 0; i < (req.params.end_id - req.params.start_id); i++) {
-        if (status[i] == false) {
-            i = 0;
-        }
-    }
-
-    console.log("result:");
-    console.log("---------------------------------------------");
-    console.log(result);
-    // res.json(result);
-    res = result;
-    //res = getPokemonInRange(req.params.start_id, req.params.end_id);
+    Pokemon.find().where('id').in(id_array)
+        .then(pokemon => res.json(pokemon))
+        .catch(err => res.status(400).json('Error:a' + err));
 });
 
 module.exports = router;
