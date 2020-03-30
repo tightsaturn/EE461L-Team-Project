b9017d42a2b2ddcd3b8eb0d58e48a2713c57055c
Jimmy Phan <jimmyphan777@gmail.com>
2020-03-29 22:18:01 -0500

    made progress in pokemon info

--- frontend/src/components/pokedex-page/PokemonInfo.js
+++ frontend/src/components/pokedex-page/PokemonInfo.js
@@ -1,25 +1,25 @@
 import React from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import "../../App.css"
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
-import FormControl from 'react-bootstrap/FormControl';
-import Table from 'react-bootstrap/Table';
-import {Link} from "react-router-dom";
-import PokemonBox from "./PokemonBox";
+import MovesBox from "../moves-page/MovesBox";
 
 const tableabilities = {
     marginTop: "70px",
     paddingLeft: "200px",
-    paddingRight: "120px"
+    paddingRight: "120px",
+    border: "blue solid 2px"
 };
 
 class PokemonInfo extends React.Component {
     constructor(){
         super();
         this.state = {
-            pokemon: {},
+            name: "",
+            imgURL: "",
+            moves: [],
+            types: []
         }
+
         this.capitalize = this.capitalize.bind(this)
     }
 
@@ -29,16 +29,22 @@ class PokemonInfo extends React.Component {
 
         fetch(url)
             .then((response) => {
-                if(response.ok){
-                    return response.json();
-                } else {
-                    throw new Error("failed to get response");
-                }
+                return response.json();
             })
             .then(data => {
-                // get page number and update state
-                this.setState({ pokemon: data})
-                console.log(data)
+                let movesArray = []
+                let typesArray = []
+
+                let type2 = data.types.length == 2 ? data.types[1].type.name : ""
+                typesArray.push(type2)
+                typesArray.push(data.types[0].type.name)
+
+                this.setState({
+                    name: data.name,
+                    imgURL: data.sprites.front_default,
+                    moves: movesArray,
+                    types: typesArray
+                })
             })
             .catch((err) =>{
                 console.log(err)
@@ -54,16 +60,47 @@ class PokemonInfo extends React.Component {
     render() {
         let id = this.props.match.params.id
 
+
         return (
-            <div className="container-fluid" style={tableabilities} align={"center"}>
-                <div className="card">
-                    <div className="col-md-5 p-lg-5 mx-auto my-5">
-                        <h1 id="pname" className="display-4 font-weight-normal">{this.capitalize(this.state.pokemon.name)}</h1>
-                        <p id="pinfo" className="lead font-weight-normal"></p>
-                        <Link to={"/pokemon/" + id + "/location"}>
-                            <a className="btn btn-outline-secondary" href="#">Location</a>
-                        </Link>
+            <div className="container-fluid" style={tableabilities} >
+                <div className="row">
+                    <div className="col-4">
+                        <div className="card" style={{width: "25rem", marginLeft: "30px"}}>
+                            <img className="card-img-top" src={this.state.imgURL} alt="Card image cap"/>
+                                <div className="card-body">
+                                    <h5 className="card-title"># {id}</h5>
+                                    <p className="card-text">{this.capitalize(this.state.name)} <br/>
+                                        description here
+                                    </p>
+                                </div>
+                                <ul className="list-group list-group-flush">
+                                    <li className="list-group-item">Types: {this.capitalize(this.state.types[0])} {this.capitalize(this.state.types[1])}</li>
+                                    <li className="list-group-item">Dapibus ac facilisis in</li>
+                                    <li className="list-group-item">Vestibulum at eros</li>
+                                </ul>
+                                <div className="card-body">
+                                    <a href="#" className="card-link">Card link</a>
+                                    <a href="#" className="card-link">Another link</a>
+                                </div>
+                        </div>
                     </div>
+
+                    <div className="col-8">
+                        <table className="table">
+                            <thead className="thead-light">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Level Learned</th>
+                                <th scope="col">Type</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+
+                            </tbody>
+                        </table>
+                    </div>
+
                 </div>
             </div>
         );
